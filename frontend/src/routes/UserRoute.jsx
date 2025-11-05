63b25717614022ac5d8fef268f867311195624b6
Edward_Bacara <tutorship57@gmail.com>
2025-11-05 14:59:42 +0700

    feat:create dockerfile

--- frontend/src/routes/UserRoute.jsx
+++ frontend/src/routes/UserRoute.jsx
@@ -1,63 +1,67 @@
-import React,{ useContext, useState,useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
-import { useAuth } from '../contextProvider/AuthProvider'
+import { useAuth } from "../contextProvider/AuthProvider";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../layout/Navbar";
-import { twoStepTryFetch } from "../services/apiCallwithToken" 
-import { fetchAccessToken ,fetchTestToken} from "../functions/accessTokenFetch";
+import { twoStepTryFetch } from "../services/apiCallwithToken";
+import {
+  fetchAccessToken,
+  fetchTestToken,
+} from "../functions/accessTokenFetch";
 import { getUserProfile } from "../functions/userManage";
 import BreadCrums from "../components/BreadCrums";
 const UserRoute = () => {
-    const { accessToken , setAccessToken } = useAuth();
-    const navigate = useNavigate();
-    const [userProfile,setUserProfile] = useState(null)
-    const fetchUserProfile = async () => {
-        try {
-            console.log("this is accessToken",accessToken)
-            const resUserProfile = await twoStepTryFetch(getUserProfile,{},accessToken,setAccessToken)
-            console.log("this is resUserProfile",resUserProfile.data.userProfile)
-            setUserProfile(resUserProfile.data.userProfile)
-
-           
-        } catch (error) {
-            console.log(error)
-        }
+  const { accessToken, setAccessToken } = useAuth();
+  const navigate = useNavigate();
+  const [userProfile, setUserProfile] = useState(null);
+  const fetchUserProfile = async () => {
+    try {
+      console.log("this is accessToken", accessToken);
+      const resUserProfile = await twoStepTryFetch(
+        getUserProfile,
+        {},
+        accessToken,
+        setAccessToken
+      );
+      console.log("this is resUserProfile", resUserProfile.data.userProfile);
+      setUserProfile(resUserProfile.data.userProfile);
+    } catch (error) {
+      console.log(error);
     }
-    const refreshToken = async()=>{
-            if(accessToken==null){
-                try {
-                    const res = await fetchAccessToken()
-                    const newToken = res.data.accessToken;
-                    if(newToken){
-                        setAccessToken(newToken); 
-                    }else {
-                        return navigate('/SignIn'); // ถ้า refresh ไม่ได้ ให้ไป login ใหม่
-                    }
-                } catch (error) {
-                    return navigate('/SignIn');
-                }
-            }
-    }
-    useEffect(() => {    
-        refreshToken()
-    }, [accessToken, navigate]);
-    useEffect(() => {
-        if(accessToken){
-            fetchUserProfile()
+  };
+  const refreshToken = async () => {
+    if (accessToken == null) {
+      try {
+        const res = await fetchAccessToken();
+        const newToken = res.data.accessToken;
+        if (newToken) {
+          setAccessToken(newToken);
+        } else {
+          return navigate("/SignIn"); // ถ้า refresh ไม่ได้ ให้ไป login ใหม่
         }
-    }, [accessToken]);
+      } catch (error) {
+        return navigate("/SignIn");
+      }
+    }
+  };
+  useEffect(() => {
+    refreshToken();
+  }, [accessToken, navigate]);
+  useEffect(() => {
+    if (accessToken) {
+      fetchUserProfile();
+    }
+  }, [accessToken]);
 
-    return (accessToken?
-        <div className="min-h-screen bg-gray-50">
-            <Navbar userProfile={userProfile}/>
-            <BreadCrums/>
-            <Outlet />
-        </div>:
-        <div>
-            Loading...
-        </div>
-      )
+  return accessToken ? (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar userProfile={userProfile} />
+      <BreadCrums />
+      <Outlet />
+    </div>
+  ) : (
+    <div>Loading...</div>
+  );
+};
 
-}
-
-export default UserRoute
+export default UserRoute;
